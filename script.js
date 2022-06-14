9ccfe3e6c034d71e611f7c7084ba9efb87d7b119
Nibague <nikolasibagues@gmail.com>
2022-06-14 13:47:42 -0500

    render the list items to the HTML with createList function

--- script.js
+++ script.js
@@ -2,5 +2,38 @@ const draggable_list = document.getElementById("draggable-list")
 const check = document.getElementById("check");
 
 const topCurriencies = [
-    
+    'BTC-USD',
+    'ETH-USD',
+    'USDT-USD',
+    'USDC-USD',
+    'BNB-USD',
+    'BUSD-USD',
+    'ADA-USD',
+    'XRP-USD',
+    'SOL-USD',
+    'DOGE-USD'
 ]
+
+const listItems = [];
+let dragStartIndex;
+
+createList();
+
+function createList(){
+    [...topCurriencies].forEach((coins, index)=>{
+        const listItem = document.createElement('li');
+
+        listItem.setAttribute('data-index', index);
+        listItem.innerHTML = `
+        <span class="number">${index + 1}</span>
+            <div class="dragable dragable = true">
+                <p class="coin-name">${coins}</p> 
+                <i class="fa-solid fa-grip-lines"></i>
+            </div>
+        `;
+
+        listItems.push(listItem);
+        draggable_list.appendChild(listItem);
+
+    })
+}
