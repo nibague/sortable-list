e0647af2f7c6c77594df5fef8e3e97bf49de0df2
Nibague <nikolasibagues@gmail.com>
2022-06-14 21:53:29 -0500

    adding functions for the eventListener dragover, drop, dragenter, dragleave

--- script.js
+++ script.js
@@ -16,6 +16,8 @@ const topCurrencies = [
 
 const listItems = [];
 
+let dragStartIndex;
+
 createList()
 
 function createList(){
@@ -43,20 +45,44 @@ function createList(){
 };
 
 function dragStart(){
-    console.log('Event:', 'dragStart');
+    //console.log('Event:', 'dragStart');
+    dragStartIndex = +this.closest('li').getAttribute('data-index');
+    console.log(dragStartIndex)
 }
 function dragEnter(){
-    console.log('Event:', 'dragEnter');
+    //console.log('Event:', 'dragEnter');
+    this.classList.add('over');
 }
 function dragLeave(){
-    console.log('Event:', 'dragLeave');
+    //console.log('Event:', 'dragLeave');
+    this.classList.remove('over');
 }
-function dragOver(){
-    console.log('Event:', 'dragOver');
+function dragOver(e){
+    //console.log('Event:', 'dragOver');
+    e.preventDefault();
 }
 function dragDrop(){
-    console.log('Event:', 'dragDrop');
+    //console.log('Event:', 'dragDrop');
+    const dragEndIndex = +this.getAttribute('data-index');
+
+    swapItems(dragStartIndex, dragEndIndex);
+
+    this.classList.remove('over');
 }
+
+function swapItems(fromIndex, toIndex){
+    const itemOne = listItems[fromIndex].querySelector('.draggable');
+    const itemTwo = listItems[toIndex].querySelector('.draggable');
+
+    listItems[fromIndex].appendChild(itemTwo);
+    listItems[toIndex].appendChild(itemOne);
+    console.log(itemOne)
+    console.log(itemTwo)
+    console.log(listItems[fromIndex])
+    console.log(listItems[toIndex])
+
+}
+
 
 function addEventListener(){
     const draggables = document.querySelectorAll('.draggable');
